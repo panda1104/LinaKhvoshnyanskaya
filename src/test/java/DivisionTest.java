cd6954fff676eeb5354fca9e2d1a2e8bd28e26c5
panda1104 <panda.khv@yandex.ru>
2019-03-22 23:48:18 +0300

    22.03

---
+++ src/test/java/DivisionTest.java
@@ -0,0 +1,64 @@
+
+import org.junit.*;
+
+
+
+import com.epam.tat.module4.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class DivisionTest {
+
+    Calculator calculator = new Calculator();
+
+
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("DivisionTest init");
+    }
+
+    @AfterClass
+    public  static void afterClass() {
+        System.out.println("Test complete");
+    }
+
+    @Before
+    public void initTest() {
+        calculator = new Calculator();
+    }
+
+    @After
+    public void afterTest() {
+        calculator = null;
+    }
+
+    @Test(groups = "MultAndDivTest")
+    public void TestDiv1()
+    {
+        Assert.assertEquals(9,calculator.div(27,3));
+    }
+
+
+    @Test(groups = "MultAndDivTest")
+    public void TestDiv2()
+    {
+        Assert.assertEquals(10,calculator.div(100,10));
+    }
+
+    @Test(groups = "MultAndDivTest")
+    public void TestDiv3()
+    {
+        Assert.assertEquals(6,calculator.div(66,11));
+    }
+
+    @Test(groups = "MultAndDivTest")
+    public void TestDiv4()
+    {
+        Assert.assertEquals(75,calculator.div(1500,20));
+    }
+
+
+}
